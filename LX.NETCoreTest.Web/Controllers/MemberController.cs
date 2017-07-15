6ef26b3a9c82610f46c6f2dc91a8af350584b24a
丶 Clearlove <gk1213656215@outlook.com>
2017-07-15 11:21:30 +0800

    第一次提交。

---
+++ LX.NETCoreTest.Web/Controllers/MemberController.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LX.NETCoreTest.Model.PyClass;
+using LX.NETCoreTest.Model.Models;
+using static LX.NETCoreTest.Common.EnumHelper;
+using LX.NETCoreTest.Common;
+
+namespace LX.NETCoreTest.Web.Controllers {
+    public class MemberController : Controller {
+
+        private readonly PyStudio_NetCoreContext _context;
+
+        /// <summary>
+        /// ×¢²á
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Register() {
+            return View();
+        }
+
+        /// <summary>
+        /// ×¢²áPOST
+        /// </summary>
+        /// <param name="loginUser"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([Bind("UserName,UserPwd,ComfirmPwd")] PyRegisterUser loginUser) {
+            if (ModelState.IsValid) {
+                #region ÑéÖ¤
+                if (_context.ToUserInfo.Any(b => b.UserName.ToUpper() == loginUser.UserName.Trim().ToUpper())) {
+                    this.MsgBox("ÒÑ¾­´æÔÚÏàÍ¬µÄÕËºÅ£¡");
+                    return View(loginUser);
+                }
+                #endregion
+
+                #region Create
+                ToUserInfo userInfo = new ToUserInfo();
+                userInfo.UserName = loginUser.UserName.Trim();
+                userInfo.UserPwd = PublicClass._Md5(loginUser.UserPwd.Trim());
+                userInfo.NickName = userInfo.UserName;
+                userInfo.Status = (int)EmUserStatus.ÆôÓÃ;
+                userInfo.CreateTime = DateTime.Now;
+                userInfo.LevelNum = (int)EmLevelNum.×¢²á;
+                userInfo.Ips = this.GetUserIp();
+                userInfo.HeadPhoto = "/images/pystudio.png";
+                userInfo.Sex = false;
+                _context.Add(userInfo);
+
+                var result = await _context.SaveChangesAsync();
+
+                if (result > 0) {
+                    var pyUserInfo = new PyUserInfo {
+                        Id = userInfo.Id,
+                        UserName = userInfo.UserName,
+                        NickName = userInfo.NickName,
+                        Addr = userInfo.Addr,
+                        Birthday = userInfo.Birthday,
+                        Blog = userInfo.Blog,
+                        CreateTime = userInfo.CreateTime,
+                        Email = userInfo.Email,
+                        HeadPhoto = userInfo.HeadPhoto,
+                        Introduce = userInfo.Introduce,
+                        Ips = userInfo.Ips,
+                        LevelNum = userInfo.LevelNum,
+                        Sex = userInfo.Sex,
+                        Tel = userInfo.Tel,
+                        Status = userInfo.Status,
+                        LoginTime = DateTime.Now
+                    };
+                    HttpContext.Session.Set<PyUserInfo>(HttpContext.Session.SessionKey(), pyUserInfo);
+
+                    if (!string.IsNullOrWhiteSpace(pyUserInfo.Ips)) {
+                        _context.ToUserLog.Add(new ToUserLog {
+                            CodeId = (int)EmLogCode.µÇÂ¼,
+                            CreateTime = DateTime.Now,
+                            Des = $"IP:{pyUserInfo.Ips},µÇÂ¼Ê±¼ä:{pyUserInfo.LoginTime.ToString("yyyy-MM-dd HH:mm:ss")}",
+                            UserId = userInfo.Id
+                        });
+                    }
+
+                    _context.ToUserLog.Add(new ToUserLog {
+                        CodeId = (int)EmLogCode.»ý·Ö,
+                        CreateTime = DateTime.Now,
+                        Des = $"¡¾×¢²á¡¿+{ (int)EmLevelNum.×¢²á}",
+                        UserId = userInfo.Id
+                    });
+
+                    await _context.SaveChangesAsync();
+
+                    return RedirectToAction(nameof(HomeController.Index), "Home");
+                }
+                #endregion
+
+                this.MsgBox("×¢²áÊ§°Ü£¬ÇëÉÔºóÖØÊÔ¡£");
+                return View(loginUser);
+            }
+            return View(loginUser);
+        }
+    }
+}
---
+++ LX.NETCoreTest.Web/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using LX.NETCoreTest.Model.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using LX.NETCoreTest.Model.PyClass;
+
+namespace LX.NETCoreTest.Web
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) {
+
+            //添加数据库上下文
+            services.AddDbContext<PyStudio_NetCoreContext>(b =>
+            {
+
+                var dbLink = Configuration.GetSection("PySelfSetting:DbLink").Value;
+                if (string.IsNullOrWhiteSpace(dbLink)) { throw new Exception("未找到数据库链接。"); }
+
+                b.UseSqlServer(dbLink);
+
+            });
+
+            services.Configure<PySelfSetting>(Configuration.GetSection("PySelfSetting"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
