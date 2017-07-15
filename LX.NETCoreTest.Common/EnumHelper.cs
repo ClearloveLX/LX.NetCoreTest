31824fdc63207c288338e51f553ed87d5cd2ece4
丶 Clearlove <gk1213656215@outlook.com>
2017-07-15 21:07:27 +0800

    Revert "Revert "第一次提交。""
    This reverts commit 895790a74ce631ea5db8e98b658310f9e6c0fce6.

---
+++ LX.NETCoreTest.Common/EnumHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LX.NETCoreTest.Common {
+    /// <summary>
+    /// 枚举支援类
+    /// </summary>
+    public class EnumHelper {
+        /// <summary>
+        /// 会员状态
+        /// </summary>
+        public enum EmUserStatus {
+            禁用 = 0, 启用 = 1
+        }
+
+        /// <summary>
+        /// 日志类型
+        /// </summary>
+        public enum EmLogCode {
+            普通 = 0,
+            登录 = 1,
+            积分 = 2
+        }
+
+        /// <summary>
+        /// 积分增加规则
+        /// </summary>
+        public enum EmLevelNum {
+            登录 = 1,
+            注册 = 2,
+            修改头像 = 3,
+            点赞 = 4,
+            上传图片 = 5,
+
+            绑定邮箱 = 6,
+            绑定手机号码 = 7
+        }
+    }
+}
